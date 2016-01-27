ea198192d1a618ac2738a4120b5c160e2150581f
Alvishuang <andysharon@gmail.com>
2016-01-28 02:31:24 +0800

    建立WebForm Folder

---
+++ TPIA/Common/Convertor/ListToDataTable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace TPIA.Common.Convertor
+{
+    public  class ListToDataTable
+    {
+        public static DataTable ConvertToDataTable<T>(IList<T> data)
+        {
+            System.ComponentModel.PropertyDescriptorCollection properties =
+               System.ComponentModel.TypeDescriptor.GetProperties(typeof(T));
+            DataTable table = new DataTable();
+            foreach (System.ComponentModel.PropertyDescriptor prop in properties)
+                table.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
+            foreach (T item in data)
+            {
+                DataRow row = table.NewRow();
+                foreach (System.ComponentModel.PropertyDescriptor prop in properties)
+                    row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
+                table.Rows.Add(row);
+            }
+            return table;
+
+        }
+    }
+}
---
+++ TPIA/FrondEnd/NewsActivity.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TPIA.Common.Convertor;
+using TPIA.Common.DTO.News;
+
+namespace TPIA.FrondEnd
+{
+    public partial class NewsActivity : System.Web.UI.Page
+    {
+        private TPIA.Common.Adaptor.TLApiAdaptor _apiAdaptor = new TPIA.Common.Adaptor.TLApiAdaptor();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetData();
+            }
+        }
+
+        private void GetData() {
+            string url = "api/News/GetNewsTitleList";
+            List<GetNewsListReturnDTO> resultDto = _apiAdaptor.Get<List<GetNewsListReturnDTO>>(url);
+
+            DataTable dt = new DataTable();
+            dt = ListToDataTable.ConvertToDataTable(resultDto);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+
+    }
+}
